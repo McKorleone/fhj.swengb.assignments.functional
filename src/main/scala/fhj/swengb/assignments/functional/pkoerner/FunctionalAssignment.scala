fb34c5fe2f95cedd7aab5f886951b55d5aaa22a9
McKorleone <paul.koerner@edu.fh-joanneum.at>
2015-10-31 10:24:49 +0100

    Finished implementation

--- src/main/scala/fhj/swengb/assignments/functional/pkoerner/FunctionalAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/functional/pkoerner/FunctionalAssignment.scala
@@ -19,12 +19,13 @@ case class FunctionalAssignment(lowerBound: Int = 1,
     * xs(i) + factor == xs(i + 1) (for i > 0 and i <= 1000)
     *
     */
-  val xs: Seq[Int] = ???
+  val xs: Seq[Int] = (lowerBound to upperBound)
 
   /**
     * provide the sum of all elements defined in xs
+    * you can use sum, fold and reduce.
     */
-  val sumXs = ???
+  val sumXs = xs.foldRight(0)(_+_)
 
   /**
     * This sequence contains a list with the following elements:
@@ -39,23 +40,26 @@ case class FunctionalAssignment(lowerBound: Int = 1,
     * x(999) = lowerbound
     *
     */
-  val ys: Seq[Int] = ???
+  val ys: Seq[Int] = xs.reverse.map(_ * factor)
 
   /**
     * this sequence contains pairs of xs and ys. the ys should
     * be in reversed order.
+    *
+    * also possible without the dot after xs and the brackets after zip
+    * but for (my) better reading with everything added
     */
-  val zs: Seq[(Int, Int)] = ???
+  val zs: Seq[(Int, Int)] = xs.zip(ys.reverse)
 
   /**
     * Function should divide the second operand with the first.
     */
-  def f(ab: (Int, Int)): Int = ???
+  def f(ab: (Int, Int)): Int = ab._2 / ab._1
 
   /**
     * apply function f defined above to all elements of zs, return the sequence
     */
-  val result: Seq[Int] = ???
+  val result: Seq[Int] = zs.map(f)
 
 }
 
